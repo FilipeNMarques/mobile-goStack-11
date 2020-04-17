017c47ca54e617139ac7e56fc5372e3f592aaa7a
Filipe <filipi.marques@gmail.com>
2020-04-17 19:32:09 -0300

    Call api data using axios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, Text, FlatList, StyleSheet, StatusBar } from 'react-native'
+
+import api from './services/api'
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+      console.log("App -> response.data", response.data)
+    })
+  }, [])
   return (
     <>
     <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-    <View  style={styles.container}>
-      <Text style={styles.title}>Hello GoStack</Text>
-    </View>
+    {/* <View  style={styles.container}>
+      {
+        repositories.map(repository => (
+          <Text key={repository.id} style={styles.repositories}>
+            {repository.title}
+          </Text>
+        ))
+      }
+    </View> */}
+
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+            data={repositories} // Need to be a array
+            keyExtractor={repository => repository.id}
+            renderItem={({item: repository}) => (
+              <Text style={styles.repositories}>
+                {repository.title}
+              </Text>
+            )}
+          />
+    </SafeAreaView>
     </>
   )
 }
@@ -15,13 +43,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#7159c1'
   },
-  title: {
-    fontSize: 42,
-    fontWeight: 'bold',
+  repositories: {
+    fontSize: 30,
     color: '#fff',
     flexDirection: 'column',
   }
